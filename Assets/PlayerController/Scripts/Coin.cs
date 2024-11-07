6c6fc90d6d788e4f3034859a52dfd397f03c7242
liamlennon <71649853+liamlennon@users.noreply.github.com>
2024-11-07 18:37:45 +0000

    Week 6
    Changed box collider to on trigger Player Prefab
    Added circle collider to Coin
    Commented out code

--- Assets/PlayerController/Scripts/Coin.cs
+++ Assets/PlayerController/Scripts/Coin.cs
@@ -6,25 +6,25 @@ using UnityEngine.UI;
 public class Coin : MonoBehaviour
 {
     [SerializeField] private int m_ScorePoint;
-    [SerializeField] private TextMeshProUGUI m_CollectableText;
-    private AudioSource m_Collected;
-    [SerializeField] GameObject m_CoinParticle;
+    //[SerializeField] private TextMeshProUGUI m_CollectableText;
+    //private AudioSource m_Collected;
+//    [SerializeField] GameObject m_CoinParticle;
 
-    private void Start()
+   /* private void Start()
     {
         m_Collected = GetComponent<AudioSource>();
-    }
+    }*/
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameManager.OnGainPoints?.Invoke(m_ScorePoint);
-        m_CollectableText.text = m_ScorePoint.ToString();
+       // m_CollectableText.text = m_ScorePoint.ToString();
         //Instantiate(m_CoinParticle, transform.position, Quaternion.identity);
         Destroy(gameObject);
-        OnDestroy();
+       // OnDestroy();
     }
     private void OnDestroy()
     {
-        m_Collected.Play();
+        //m_Collected.Play();
     }
 }
