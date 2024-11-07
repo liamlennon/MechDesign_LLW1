dfbb859117f5dfe4168b914f78d3ca9aef977e18
liamlennon <71649853+liamlennon@users.noreply.github.com>
2024-11-07 12:10:01 +0000

    Week 5 Thursday 1
    Merge Changes

--- Assets/PlayerController/Scripts/HealthComponent.cs
+++ Assets/PlayerController/Scripts/HealthComponent.cs
@@ -11,12 +11,16 @@ public class HealthComponent : MonoBehaviour, IDamageable
     public GameObject Player;
 
     [SerializeField] private Image m_HealthBar;
-    [SerializeField] private Image m_LowHealthEffect;
     [SerializeField] private bool m_HasHealthBar;
     [SerializeField] private float m_MaxHealth;
     [SerializeField] private float m_CurrentHealth;
+<<<<<<< Updated upstream
     [SerializeField] private float damageAlpha = 0.3f, damageFadeSpeed = 3f;
 
+=======
+    [SerializeField] private Image m_HealthEffect;
+    [SerializeField] private float damageAlpha = 0.3f, damageFadeSpeed = 3f; 
+>>>>>>> Stashed changes
 
     private void Awake()
     {
@@ -29,11 +33,18 @@ public class HealthComponent : MonoBehaviour, IDamageable
             m_HealthBar.fillAmount = m_CurrentHealth / 100;
         }
 
+<<<<<<< Updated upstream
         if (m_LowHealthEffect.color.a != 0)
         {
             m_LowHealthEffect.color = new Color(m_LowHealthEffect.color.r, m_LowHealthEffect.color.g, m_LowHealthEffect.color.b, Mathf.MoveTowards(m_LowHealthEffect.color.a, 0f,
             damageFadeSpeed * Time.deltaTime));
         }
+=======
+        if(m_HealthEffect.color.a != 0)
+        {
+            m_HealthEffect.color = new Color(m_HealthEffect.color.r, m_HealthEffect.color.g, m_HealthEffect.color.b, Mathf.MoveTowards(m_HealthEffect.color.a, 0f, damageFadeSpeed * Time.deltaTime));
+        } 
+>>>>>>> Stashed changes
     }
 
     public void ApplyDamage(float damage, MonoBehaviour causer)
@@ -47,9 +58,23 @@ public class HealthComponent : MonoBehaviour, IDamageable
 
        
 
+<<<<<<< Updated upstream
     }
     public void ShowDamage()
     {
         m_LowHealthEffect.color = new Color(m_LowHealthEffect.color.r, m_LowHealthEffect.color.g, m_LowHealthEffect.color.b, 0.3f);
     }
+=======
+        if (m_MaxHealth < 10)
+        {
+            m_HealthEffect.color = new Color(m_HealthEffect.color.r, m_HealthEffect.color.g, m_HealthEffect.color.b, Mathf.MoveTowards(m_HealthEffect.color.a, 0f,
+            damageFadeSpeed * Time.deltaTime));
+        } 
+    }
+    public void ShowDamage()
+    {
+        m_HealthEffect.color = new Color(m_HealthEffect.color.r, m_HealthEffect.color.g, m_HealthEffect.color.b, 0.3f);
+    }                 
+
+>>>>>>> Stashed changes
 }
